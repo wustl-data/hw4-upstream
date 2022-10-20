e193086d57f1f8e0190a3d87b882df963a019543
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2022-10-20 04:34:16 +0000

    add named index

--- stacksurvey.py
+++ stacksurvey.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 def median_by_undergrad(survey: pd.DataFrame) -> pd.Series:
-    return pd.Series(name="Median Salary")
+    return pd.Series(name="Median Salary", index=pd.Index([], name="Undergraduate Major"))
